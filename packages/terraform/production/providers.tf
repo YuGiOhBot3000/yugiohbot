b75e0fa6ce3362df08f471dee17452a954dfec41
Miles Bardon <milesbardon@gmail.com>
2022-02-02 23:13:03 +0000

    Adding deployment step to CD pipeline

--- packages/terraform/production/providers.tf
+++ packages/terraform/production/providers.tf
@@ -1,3 +1,12 @@
 provider "aws" {
-  region = "eu-central-1"
+  region  = "eu-central-1"
+  profile = "yugiohbot"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "yugiohbot-tf-state"
+    key    = "terraform.tfstate"
+    region = "eu-central-1"
+  }
 }
