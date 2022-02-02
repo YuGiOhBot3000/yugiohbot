09429a94a33de9972e4dd6788b08327d2fe49314
Miles Bardon <milesbardon@gmail.com>
2022-02-02 16:56:49 +0000

    Adding createTitle function and tests

--- packages/generate-title/src/title.ts
+++ packages/generate-title/src/title.ts
@@ -8,6 +8,18 @@ type TitleParts = {
 const LANGUAGE = "EN";
 const DEFAULT_CATEGORY = "N";
 
+/**
+ * Generates a random integer between min and max inclusive
+ * @param min Start of the range
+ * @param max End of the range
+ * @returns Random integer
+ */
+const randomInt = (min: number, max: number) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomElement = (arr: string[]) =>
+  arr[Math.floor(Math.random() * arr.length)];
+
 export const parseTitles = (titles: string[]): TitleParts => {
   const lexicon = new Lexicon(LANGUAGE, DEFAULT_CATEGORY);
   const ruleSet = new RuleSet(LANGUAGE);
@@ -36,4 +48,37 @@ export const parseTitles = (titles: string[]): TitleParts => {
     nouns: [...new Set(titleParts.nouns)],
     adjectives: [...new Set(titleParts.adjectives)],
   };
+};
+
+export const createTitle = ({ nouns, adjectives }: TitleParts) => {
+  if (!nouns.length && !adjectives.length) {
+    console.warn("No title parts found, aborting");
+    return "";
+  }
+
+  const title = [];
+  const connectingWords = ["of", "of the", "the"];
+
+  // 1. Decide number of sections the title will have
+  const sections = randomInt(1, 2);
+
+  // 2. Create a new title component for each section
+  for (let i = 0; i < sections; i++) {
+    // 3. Decide number of parts this component will have
+    const components = randomInt(1, 2);
+
+    // 4. Create the component (noun or adjective + noun)
+    if (components === 1) {
+      title.push(randomElement(nouns));
+    } else {
+      title.push(randomElement(adjectives), randomElement(nouns));
+    }
+
+    // 5. Add connecting words if it's not the last component
+    if (i + 1 < sections) {
+      title.push(randomElement(connectingWords));
+    }
+  }
+
+  return title.join(" ");
 };
--- packages/generate-title/tests/title.test.ts
+++ packages/generate-title/tests/title.test.ts
@@ -1,4 +1,6 @@
-import { parseTitles } from "../src/title";
+import { createTitle, parseTitles } from "../src/title";
+
+const warnSpy = jest.spyOn(console, "warn").mockImplementation(jest.fn());
 
 describe("Title", () => {
   const mockTitles = [
@@ -8,12 +10,76 @@ describe("Title", () => {
     "dark magician 2",
   ];
 
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   describe("parseTitles", () => {
     it("should return lists of nouns and adjectives", () => {
       const { nouns, adjectives } = parseTitles(mockTitles);
 
       expect(nouns).toEqual(["magician", "eyes", "dragon", "pot"]);
       expect(adjectives).toEqual(["dark", "blue", "white"]);
+    });
+
+    describe("given the titles list is empty", () => {
+      it("should return empty lists of nouns and adjectives", () => {
+        const { nouns, adjectives } = parseTitles([]);
+
+        expect(nouns).toEqual([]);
+        expect(adjectives).toEqual([]);
+      });
+    });
+  });
+
+  describe("createTitle", () => {
+    describe("given the title has one part", () => {
+      beforeEach(() => {
+        jest
+          .spyOn(Math, "random")
+          .mockReturnValueOnce(0.25) // Return 1 for sections
+          .mockReturnValueOnce(0.25) // Return 1 for first component
+          .mockReturnValueOnce(0.25); // Return 1 for 2nd noun
+      });
+
+      it("should return a random title", () => {
+        const nouns = ["magician", "eyes", "dragon", "pot"];
+        const adjectives = ["dark", "blue", "white"];
+
+        const result = createTitle({ nouns, adjectives });
+
+        expect(result).toEqual("eyes");
+      });
+    });
+
+    describe("given the title has two parts", () => {
+      beforeEach(() => {
+        jest
+          .spyOn(Math, "random")
+          .mockReturnValueOnce(0.5) // Return 2 for sections
+          .mockReturnValueOnce(0.5) // Return 2 for first component
+          .mockReturnValueOnce(1 / 3) // Return 1 for 2nd adjective
+          .mockReturnValueOnce(0) // Return 0 for 1st noun
+          .mockReturnValueOnce(0) // Return 0 for 1st connecting word
+          .mockReturnValueOnce(0.25) // Return 1 for second component
+          .mockReturnValueOnce(0.5); // Return 2 for 3rd noun
+      });
+
+      it("should return a random title with a connecting word", () => {
+        const nouns = ["magician", "eyes", "dragon", "pot"];
+        const adjectives = ["dark", "blue", "white"];
+
+        const result = createTitle({ nouns, adjectives });
+
+        expect(result).toEqual("blue magician of dragon");
+      });
+    });
+
+    describe("given the nouns and adjectives lists are both empty", () => {
+      it("should log a warning and return an empty string", () => {
+        expect(createTitle({ nouns: [], adjectives: [] })).toEqual("");
+        expect(warnSpy).toBeCalledTimes(1);
+      });
     });
   });
 });
