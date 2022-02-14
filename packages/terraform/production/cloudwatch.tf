021e307c05917353d1e623a825b7776791c68ba9
Miles Bardon <milesbardon@gmail.com>
2022-02-14 22:08:08 +0000

    Adding deployment config

--- packages/terraform/production/cloudwatch.tf
+++ packages/terraform/production/cloudwatch.tf
@@ -9,3 +9,15 @@ resource "aws_cloudwatch_event_target" "run_bot_every_hour" {
   arn      = aws_sfn_state_machine.state_machine.arn
   role_arn = aws_iam_role.invoke_sfn_role.arn
 }
+
+resource "aws_cloudwatch_event_rule" "every_week" {
+  name                = "booster-pack-every-week"
+  description         = "Fires once a week"
+  schedule_expression = "cron(23 0 * * 6)"
+}
+
+resource "aws_cloudwatch_event_target" "run_booster_pack_every_week" {
+  rule     = aws_cloudwatch_event_rule.every_week.name
+  arn      = module.booster_pack_lambda.lambda_arn
+  role_arn = aws_iam_role.invoke_sfn_role.arn
+}
