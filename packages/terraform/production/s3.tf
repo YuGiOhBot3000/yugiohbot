8eee51da2570d2969c603e01793436c28d63054c
Miles Bardon <milesbardon@gmail.com>
2022-02-19 16:08:50 +0000

    Adding bucket notification to public bucket

--- packages/terraform/production/s3.tf
+++ packages/terraform/production/s3.tf
@@ -62,3 +62,22 @@ resource "aws_s3_bucket" "private_submission_bucket" {
   bucket = "${var.app_name}-private-submissions"
   acl    = "private"
 }
+
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = module.moderate_image_lambda.lambda_function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.public_submission_bucket.arn
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.public_submission_bucket.id
+
+  lambda_function {
+    lambda_function_arn = module.moderate_image_lambda.lambda_arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [aws_lambda_permission.allow_bucket]
+}
--- packages/terraform/user/main.tf
+++ packages/terraform/user/main.tf
@@ -67,6 +67,7 @@ resource "aws_iam_group_policy" "s3" {
           "s3:ListBucket",
           "s3:PutBucketAcl",
           "s3:PutBucketPolicy",
+          "s3:PutBucketNotification",
           "s3:PutLifecycleConfiguration"
         ]
         Effect   = "Allow"
